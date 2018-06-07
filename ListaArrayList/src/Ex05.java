070bf6109436c66297fcc59c4fc8205d6821a608
lucassfreed <39159697+lucassfreed@users.noreply.github.com>
2018-06-07 11:29:24 -0300

    07/06

--- ListaArrayList/src/Ex05.java
+++ ListaArrayList/src/Ex05.java
@@ -1,6 +1,6 @@
 
 import java.util.ArrayList;
-
+import javax.swing.JOptionPane;
 
 /**
  * @author Lucas Rodrigo
@@ -8,18 +8,23 @@ import java.util.ArrayList;
 public class Ex05 {
 
     public static void main(String[] args) {
-        
+
         ArrayList<String> nomes = new ArrayList<>();
-        nomes.add("Teste");
-        nomes.add("");
-        nomes.add("");
-        nomes.add("");
-        nomes.add("");
-        nomes.add("");
-        nomes.add("");
-        nomes.add("");
-        nomes.add("");
-        
+        nomes.add(JOptionPane.showInputDialog("Informe o primeiro nome"));
+        nomes.add(JOptionPane.showInputDialog("Informe o segundo nome"));
+        nomes.add(JOptionPane.showInputDialog("Informe o terceiro nome"));
+        nomes.add(JOptionPane.showInputDialog("Informe o quarto nome"));
+
+        String outroNome = JOptionPane.showInputDialog("Informe o quinto nome");
+
+        if (nomes.get(0) == outroNome 
+                || nomes.get(1) == outroNome 
+                || nomes.get(2) == outroNome 
+                || nomes.get(3) == outroNome) {
+            System.out.println("O número está contido no ArrayList.");
+        } else {
+            System.out.println("O número não está contido no ArrayList.");
+        }
     }
 
 }
---
+++ ListaArrayList/src/Ex07.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author Lucas Rodrigo
+ */
+public class Ex07 {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
